65152f936e75f9de8888097730c5191baaa33ac3
Zaynah Redzovic <zaynahredzovic@gmail.com>
2026-02-15 22:00:57 +0100

    fixed add btn

--- ajax/addBook.php
+++ ajax/addBook.php
@@ -1,21 +1,17 @@
 <?php
 
-include_once '../init.php';
-$form = new Form();
-$queries = new Queries();
+include "../init.php";
+$form = new form;
+$queries = new queries;
+if(isset($_POST['bookName']) && isset($_POST['bookAuthor']) && isset($_POST['bookPrice'])){
 
-if(isset($_POST['bookName']) && isset($_POST['authorName']) && isset($_POST['bookPrice'])){
-    
-    $bookName = $form->input('bookName');
-    $authorName = $form->input('authorName');
-    $bookPrice = $form->input('bookPrice');
-    $userId = $_SESSION['userId'];
+    $bookName     = $form->input("bookName");
+    $bookAuthor   = $form->input('bookAuthor');
+    $bookPrice    = $form->input('bookPrice');
+    $userId       = $_SESSION['userId'];
+    if($queries->Crud("INSERT INTO books(bookName, authorName, bookPrice, userId) VALUES (?,?,?,?)", [$bookName, $bookAuthor, $bookPrice, $userId])){
 
-    if($queries->Crud('INSERT INTO books (bookName, authorName, bookPrice, userId) VALUES (?,?,?,?)', [$bookName, $authorName, $bookPrice, $userId])){
-        
-    echo json_encode(['status' => 'success', 'message' => 'Book added successfully']);
-    }else{
-        echo json_encode(['status' => 'error', 'message' => 'Failed to add book']);
+        echo json_encode(['status' => 'success', 'msg' => 'Your book has been added successfully']);
     }
 
 }
